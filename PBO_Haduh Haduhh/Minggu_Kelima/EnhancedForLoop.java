466c44d671a00820470c038ca08f49e212abd4e9
RealXEyez <132649141+KuugaDrive@users.noreply.github.com>
2024-05-18 13:33:12 +0700

    Delete PBO_Haduh Haduhh directory

--- PBO_Haduh Haduhh/Minggu_Kelima/EnhancedForLoop.java
+++
@@ -1,9 +0,0 @@
-public class EnhancedForLoop {
-    public static void main(String[]args){
-        int []number={1,2,3,4,5,6,7,8,9,10};
-        for(int item : number){
-                System.out.println("Count is : "+ item);
-        }
-        
-    }
-}
--- PBO_Haduh Haduhh/Minggu_Kelima/WhileLoop.java
+++
@@ -1,8 +0,0 @@
-public class WhileLoop {
-    public static void main(String[]args){
-        int count=1;
-        while (count<11) {
-        System.out.println("Count is : "+ count);count++;            
-        }
-    }
-}
